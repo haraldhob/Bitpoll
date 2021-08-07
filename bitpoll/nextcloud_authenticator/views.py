6207707eebdbe62e284570fbd0a910fc71fffbd3
Lukas Döllerer <lukas.doellerer@tum.de>
2021-08-07 13:30:09 +0200

    Resolve problems with nextcloud ocs API

--- bitpoll/nextcloud_authenticator/views.py
+++ bitpoll/nextcloud_authenticator/views.py
@@ -19,13 +19,15 @@ class CustomNextCloudAdapter(NextCloudAdapter):
     provider_id = CustomNextCloudProvider.id
     settings = app_settings.PROVIDERS.get(provider_id, {})
     server = settings.get("SERVER", "https://nextcloud.example.org")
-    access_token_url = "{0}/apps/oauth2/api/v1/token".format(server)
-    authorize_url = "{0}/apps/oauth2/authorize".format(server)
+    access_token_url = "{0}/index.php/apps/oauth2/api/v1/token".format(server)
+    authorize_url = "{0}/index.php/apps/oauth2/authorize".format(server)
     profile_url = "{0}/ocs/v1.php/cloud/users/".format(server)
 
     def get_user_info(self, token, user_id):
         headers = {"Authorization": "Bearer {0}".format(token)}
         resp = requests.get(self.profile_url + user_id, headers=headers)
+        if resp.status_code != 200:
+            print("Error in user metadata request: " + str(resp.status_code) + " " + resp.content.decode())
         resp.raise_for_status()
         data = ET.fromstring(resp.content.decode())[1]
 
@@ -36,6 +38,10 @@ class CustomNextCloudAdapter(NextCloudAdapter):
             # The 'groups' tag doesn't contain plain text, but a list of group strings
             if d.tag == 'groups':
                 result_dict[d.tag] = ", ".join([e.text for e in d])
+
+        # Our login fails if the nextcloud user doesn't have a valid email. This shouldn't be the case.
+        if "email" not in result_dict.keys():
+            raise ValueError(f"Missing user email in nextcloud account for user {user_id}.")
         return result_dict
 
 
